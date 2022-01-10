d260aa371a2c4e2f0b6e2585735974a685c7ba62
Nellen <nellencr@gmail.com>
2022-01-10 19:32:55 +0200

    Updates

---
+++ precache-manifest.d2a46281d7d215b849b05a2162313b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80fabacf33c713397648",
+    "url": "/_final_project/static/css/main.429a55bb.chunk.css"
+  },
+  {
+    "revision": "80fabacf33c713397648",
+    "url": "/_final_project/static/js/main.80fabacf.chunk.js"
+  },
+  {
+    "revision": "8f195c1c981e8b558bef",
+    "url": "/_final_project/static/css/1.6ad5251b.chunk.css"
+  },
+  {
+    "revision": "8f195c1c981e8b558bef",
+    "url": "/_final_project/static/js/1.8f195c1c.chunk.js"
+  },
+  {
+    "revision": "52700005f2a7df3ef049",
+    "url": "/_final_project/static/js/runtime~main.52700005.js"
+  },
+  {
+    "revision": "328a81acd4b2fca0b8d2ea58d1b41c87",
+    "url": "/_final_project/index.html"
+  }
+];
